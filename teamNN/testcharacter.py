9aaab85f0aef77710478cd4dad070446143b5141
Angelo Reade <arreade@wpi.edu>
2026-09-18 18:35:12 -0400

    Adding basic state machine skeleton to testcharacter.py

--- teamNN/testcharacter.py
+++ teamNN/testcharacter.py
@@ -5,8 +5,78 @@ sys.path.insert(0, '../bomberman')
 from entity import CharacterEntity
 from colorama import Fore, Back
 
+#import enum for RobotStates
+from enum import Enum, auto
+
 class TestCharacter(CharacterEntity):
 
     def do(self, wrld):
         # Your code here
+
+        ###########
+        #States####
+        ###########
+        class RobotStates(Enum):
+            SAFE_NAVIGATION = auto()
+            STUPID_MONSTER_IN_PROXIMITY = auto()
+            SMART_MONSTER_IN_PROXIMITY = auto()
+            BOMB_SAFETY_PROTOCOL = auto()
+           
+        #Initialize robot into the Safe Navigation state
+        ROBOT_STATE = RobotStates.SAFE_NAVIGATION
+
+        ###########
+        #StateEntry
+        ###########
+
+        def Enter_Safe_Navigation():
+            ROBOT_STATE = RobotStates.SAFE_NAVIGATION
+            pass
+        def Enter_Stupid_Monster_In_Proximity():
+            ROBOT_STATE = RobotStates.STUPID_MONSTER_IN_PROXIMITY
+            pass
+        def Enter_Smart_Monster_In_Proximity():
+            ROBOT_STATE = RobotStates.SMART_MONSTER_IN_PROXIMITY
+            pass
+        def Enter_Bomb_Safety_Protocol():
+            ROBOT_STATE = RobotStates.BOMB_SAFETY_PROTOCOL
+            pass
+        ###########
+        #SearchAlgs
+        ###########
+        def BFS():
+            
+            pass
+
+        ###########
+        #Update####
+        ###########
+        def Update():
+            if ROBOT_STATE == RobotStates.SAFE_NAVIGATION:
+                #Call BFS
+                pass
+            if ROBOT_STATE == RobotStates.STUPID_MONSTER_IN_PROXIMITY:
+                #Call Expectimax
+                pass
+            if ROBOT_STATE == RobotStates.SMART_MONSTER_IN_PROXIMITY:
+                #Call minimax
+                pass
+            if ROBOT_STATE == RobotStates.BOMB_SAFETY_PROTOCOL:
+                #Call BombSafetyProtocol 
+                pass
+
+
+        ###########
+        #Checkers##
+        ###########
+
+        ###########
+        #Handlers##
+        ###########
+
+
+
+        ###########
+        #MainLoop##
+        ###########
         pass
