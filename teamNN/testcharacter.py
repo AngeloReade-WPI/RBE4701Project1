dd69f967871bcda5b3ad0dd46af35e71245420e1
Angelo Reade <arreade@wpi.edu>
2026-09-19 18:01:04 -0400

    Implemented method to follow path generated by BFS algorithm

--- teamNN/testcharacter.py
+++ teamNN/testcharacter.py
@@ -44,10 +44,12 @@ class TestCharacter(CharacterEntity):
         ###########
         #SearchAlgs
         ###########
-        def find_neighbors(character):
+        def find_neighbors(nodeX, nodeY):
             #Get Current Position of Robot
-            Current_Position = (wrld.me(character).x, wrld.me(character).y)
+            Current_Position = (nodeX,nodeY)
             All_Neighbors = []
+            Invalid_Neighbors = []
+            Valid_Neighbors = []
             All_Neighbors.append((Current_Position[0] + 1, Current_Position[1]))
             All_Neighbors.append((Current_Position[0] + 1, Current_Position[1] + 1))
             All_Neighbors.append((Current_Position[0] + 1, Current_Position[1] - 1))
@@ -57,17 +59,17 @@ class TestCharacter(CharacterEntity):
             All_Neighbors.append((Current_Position[0] - 1, Current_Position[1] + 1))
             All_Neighbors.append((Current_Position[0] - 1, Current_Position[1] - 1))
             for neighbor in All_Neighbors:
-                if neighbor[0] < 0 or neighbor[1] < 0 or neighbor[0] >= wrld.width() or neighbor[1] >= wrld.height():
-                    All_Neighbors.remove(neighbor)
-                if wrld.wall_at(neighbor[0], neighbor[1]):
-                    All_Neighbors.remove(neighbor)
-            return All_Neighbors
-        def BFS(G,S,T):
+                if neighbor[0] < 0 or neighbor[1] < 0 or neighbor[0] >= wrld.width() or neighbor[1] >= wrld.height() or wrld.wall_at(neighbor[0], neighbor[1]):
+                    Invalid_Neighbors.append(neighbor)
+            Valid_Neighbors = [neighbor for neighbor in All_Neighbors if neighbor not in Invalid_Neighbors]
+            return Valid_Neighbors
+        def BFS(S,T):
             #Initialize a queue with starting position and path to get there
             queue = [(S, [S])]
             #Keep track of visited nodes to avoid cycles
             visited = set()
             #loop over the queue until it's empty
+            discovered = set([S])
             while queue:
                 #get the first item in the queue
                 ((current_nodeX, current_nodeY), path) = queue.pop(0)
@@ -79,11 +81,19 @@ class TestCharacter(CharacterEntity):
                     #mark the current node as visited
                     visited.add((current_nodeX, current_nodeY))
                     #add all unvisited neighbors to the queue
-                    for neighbor in G.neighbors((current_nodeX, current_nodeY)):
-                        if neighbor not in visited:
+                    for neighbor in find_neighbors(current_nodeX, current_nodeY):
+                        if neighbor not in visited and neighbor not in discovered:
                             queue.append((neighbor, path + [neighbor]))
-            
+                            discovered.add(neighbor)
+
             return None
+        
+        def follow_path(path):
+            #Follow the path given by BFS
+            current_node = path[0]
+            next_node = path[1]
+                #Move to the next node in the path
+            self.move(next_node[0] - current_node[0], next_node[1] - current_node[1])
 
         ###########
         #Update####
@@ -91,6 +101,8 @@ class TestCharacter(CharacterEntity):
         def Update():
             if ROBOT_STATE == RobotStates.SAFE_NAVIGATION:
                 #Call BFS
+                Path = BFS((wrld.me(self).x, wrld.me(self).y), wrld.exitcell)
+                follow_path(Path)
                 pass
             if ROBOT_STATE == RobotStates.STUPID_MONSTER_IN_PROXIMITY:
                 #Call Expectimax
@@ -106,19 +118,23 @@ class TestCharacter(CharacterEntity):
         ###########
         #Checkers##
         ###########
-
+        def Check_Safe_Navigation():
+            #TODO: make sure ts is safe to navigate
+            return True
         ###########
         #Handlers##
         ###########
-
+        def Handle_Safe_Navigation():
+            Enter_Safe_Navigation()
+            Update()
+            pass
 
 
         ###########
         #MainLoop##
         ###########
-        test = find_neighbors(self)
-        print(test)
-        pass
-        
-
-        
+   
+        if Check_Safe_Navigation():
+            Handle_Safe_Navigation()
+        else:
+            pass
